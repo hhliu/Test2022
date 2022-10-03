e891281fa371866a5b8f56017ac386a82c22257a
hhliu <hunghuan.liu@gmail.com>
2022-10-03 20:33:10 +0800

    作業1-1

--- app/src/main/java/cycu/nclab/proj/myapplication/MainActivity.java
+++ app/src/main/java/cycu/nclab/proj/myapplication/MainActivity.java
@@ -2,13 +2,27 @@ package cycu.nclab.proj.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tx;
+    Button bt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tx = findViewById(R.id.textView);
+        bt = findViewById(R.id.button);
+    }
+
+    public void onButtonClick(View view){
+        tx.setTextColor(Color.RED);
     }
 }
